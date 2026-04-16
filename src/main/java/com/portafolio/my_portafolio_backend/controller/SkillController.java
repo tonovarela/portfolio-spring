8a6c686cc096c6593b7b4f716be48701e2021eaf
Marco Antonio Estelles <tonovarela@live.com>
2026-04-16 08:41:45 -0600

    SkillController listo

--- src/main/java/com/portafolio/my_portafolio_backend/controller/SkillController.java
+++ src/main/java/com/portafolio/my_portafolio_backend/controller/SkillController.java
@@ -4,12 +4,15 @@ import com.portafolio.my_portafolio_backend.dto.SkillDTO;
 import com.portafolio.my_portafolio_backend.mappers.SkillMapper;
 import com.portafolio.my_portafolio_backend.model.Skill;
 import com.portafolio.my_portafolio_backend.service.interfaces.ISkillService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/skills")
@@ -34,13 +37,63 @@ public class SkillController {
 
     @GetMapping("/new")
     public String showForm(Model model) {
-        SkillDTO skillDTO = new SkillDTO();
-        model.addAttribute("skillDTO", skillDTO);
+        model.addAttribute("skillDTO", new SkillDTO());
         return "/skill/form";
     }
+    @PostMapping("/save")
+    public String saveSkill(@Valid @ModelAttribute("skillDTo") SkillDTO skillDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/skill/form";
+        }
+        try{
+           Skill skill = SkillMapper.toEntity(skillDTO);
+           skillService.save(skill);
+           return  "redirect:/skills/list";
+        }
+        catch (Exception e) {
+            System.out.println("Error saving skill: " + e.getMessage());
+            return "/error-page";
+        }
+
+    }
+
+   @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+      Optional<Skill> skillOptions =  skillService.findById(id);
+      if  (skillOptions.isEmpty()) {
+          model.addAttribute("errorMessage", "Skill not found with id: " + id);
+          return "redirect:/skills/list";
+      }
+       SkillDTO skill = SkillMapper.toDTO(skillOptions.get());
+       model.addAttribute("skillDTO", skill);
+       return  "/skill/list";
+
+   }
+   @GetMapping("/personal/{personalInfoId}")
+    public String findByPersonalInfoId(@PathVariable("personalInfoId") Long personalInfoId, Model model) {
+        List<SkillDTO> skillsDTO = skillService.findAll()
+                .stream()
+                .filter(skill -> skill.getPersonalInfoId().equals(personalInfoId))
+                .map(SkillMapper::toDTO)
+                .toList();
+        model.addAttribute("skills", skillsDTO);
+        return "/skill/list";
+   }
+
+   @PostMapping ("/delete/{skill}")
+    public String delete(@PathVariable("skill") Long skillId, RedirectAttributes redirectAttributes) {
+        try{
+            skillService.deleteById(skillId);
+            redirectAttributes.addFlashAttribute("message", "Skill deleted successfully");
+        }catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error deleting skill: " + e.getMessage());
+        }
+        return "redirect:/skills/list";
+
+   }
 
 
 
-}
+ }
 
 
